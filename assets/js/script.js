62190f44981c911c490df2d6c306019afc19b107
brandon-stewart-rgb <brandon@pixeltree.us>
2021-07-05 19:57:07 -0600

    value from taskNameInput

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,12 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    //console.dir(taskNameInput);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    listItemEl.textContent = taskNameInput;
+    //getting directly from taskNameInput object
     tasksToDoEl.appendChild(listItemEl);
     
 }
